6e2846353476b5d6e03c78e30e654430a269b6f6
Wen-Shiu Hung <catinclay@catinclay-macbookpro.roam.corp.google.com>
2020-05-23 18:50:59 -0700

    ver 1.20: convert wav to mp3

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var soundManager = new SoundManager();
 gameEngine.init(game, imageManager, soundManager, 60);
 
 soundManager.registerSound({name:'failedSound', src:'sounds/failedSound.mp3'}, 1);
-soundManager.registerSound({name:'whip-sound', src:'sounds/whip-sound.wav'}, 0.1, 1);
-soundManager.registerSound({name:'boss-hit', src:'sounds/hit.wav'}, 0.3, 1);
+soundManager.registerSound({name:'whip-sound', src:'sounds/whip-sound.mp3'}, 0.1, 1);
+soundManager.registerSound({name:'boss-hit', src:'sounds/hit.mp3'}, 0.3, 1);
 soundManager.registerSound({name:'mainball-hit', src:'sounds/punch.mp3'}, 1, 1);
-soundManager.registerSound({name:'lb-sfx', src:'sounds/lb-sfx.wav'}, 0.2, 1);
+soundManager.registerSound({name:'lb-sfx', src:'sounds/lb-sfx.mp3'}, 0.2, 1);
 soundManager.registerSound({name:'boss-battle-bgm', src:'sounds/boss-battle-bgm.mp3'}, 0.05, 1);
 
 var loadPromises = [
